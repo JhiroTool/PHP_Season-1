2ab45ba843be6054b2bb1b2b41cfef242034b183
Jhiro Tool <jhiroramir@gmail.com>
2025-08-17 08:09:39 +0800

    eyy

--- user/index.php
+++ user/index.php
@@ -4,9 +4,13 @@
     if(isset($_SESSION["id"])){
         $user_id = $_SESSION["id"];
         include("../connection.php");
+        $get_record = mysqli_query($connection, "SELECT * FROM info_tbl WHERE id = '$user_id'");
+        while($row_edit = mysqli_fetch_assoc($get_record)){
+            $db_name = $row_edit['name'];
+            $db_address = $row_edit['address'];
+            $db_email = $row_edit['email'];
+        } echo "Welcome, $db_name !" . " <a href='../logout.php'>Logout</a>";
     }else{
-        echo "You musr login first! <a href='../login.php'>Login now!</a>";
+        echo "You must login first! <a href='../login.php'>Login now!</a>";
     }
 ?>
-  git config --global user.email "jhiroramir@gmail.com"
-  git config --global user.name "Jhiro Tool"
